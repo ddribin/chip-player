182afacc21ce27fa7740a412cd3c51cd677d9b3e
Dave Dribin <dave@dribin.org>
2010-03-30 22:15:35 -0500

    Initial import

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Retro Player
+//
+//  Created by Dave Dribin on 3/30/10.
+//  Copyright 2010 Bit Maki, Inc.. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Retro Player
+//
+//  Created by Dave Dribin on 3/30/10.
+//  Copyright 2010 Bit Maki, Inc.. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
