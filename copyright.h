a0764869b73a1a5dcb1a941845c1b3742f7bfe59
Dave Dribin <dave@dribin.org>
2010-05-01 01:16:59 -0500

    Update copyright headers

--- copyright.h
+++ copyright.h
@@ -21,37 +21,3 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-#import <Cocoa/Cocoa.h>
-#import "MusicPlayerActions.h"
-
-
-@interface MockMusicPlayerActions : NSObject <MusicPlayerActions>
-{
-    int currentTrack;
-    int selectedTrack;
-    int numberOfTracks;
-
-@public
-    // Expectations
-    BOOL enableOrDisablePreviousAndNextWasCalled;
-    BOOL startAudioWasCalled;
-    BOOL stopAudioWasCalled;
-    BOOL activateAudioSessionWasCalled;
-    BOOL setButtonToPlayWasCalled;
-    BOOL setButtonToPausedWasCalled;
-}
-
-@property int currentTrack;
-@property int selectedTrack;
-@property (readonly) int numberOfTracks;
-
-+ (id)mockActions;
-
-- (void)clearExpectations;
-
-- (void)setCurrentTrackToFirstTrack;
-- (void)setCurrentTrackToMiddleTrack;
-- (void)setCurrentTrackToLastTrack;
-
-@end
